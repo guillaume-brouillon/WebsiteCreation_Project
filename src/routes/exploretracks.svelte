444b4d62216c3460aa220dbc5c8def0be6a1b14c
EK121 <99645942+EK121@users.noreply.github.com>
2022-03-20 20:21:56 +0100

    Add routes_track

---
+++ src/routes/exploretracks.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+    import { supabase } from "../supabaseClient";
+    let user = supabase.auth.user();
+    export async function load() {
+        if (user == null) {
+            return {
+                status: 302,
+                redirect: "/"
+            };
+        }
+        return {};
+    }
+</script>
+
+<script>
+    import Header from "$lib/header/Header.svelte";
+    import { trackID } from '$lib/sessionStore';
+    import SearchTrack from '$lib/search/SearchTrack.svelte';
+  
+    let trackIdRequested;
+
+    trackID.subscribe(value => {
+        trackIdRequested = value;
+    });
+</script>
+
+
+<Header />
+<div>
+    <SearchTrack />
+    {trackIdRequested}
+</div>
+
+<main>
+</main>
